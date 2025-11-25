ef01c5f7d8438e7176b19b085ce3a7dbca03a4e1
TihomirPop <tihomirpopovic42@gmail.com>
2025-11-25 15:37:41 +0100

    init

---
+++ src/main/java/hr/tpopovic/myshowlist/MyShowListApplication.java
@@ -0,0 +1,13 @@
+package hr.tpopovic.myshowlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyShowListApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(MyShowListApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hr/tpopovic/myshowlist/MyShowListApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.tpopovic.myshowlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyShowListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
