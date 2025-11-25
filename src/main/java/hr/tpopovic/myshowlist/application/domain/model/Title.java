444519019f0216ad77dc4db339ea4113d9ac82d2
TihomirPop <tihomirpopovic42@gmail.com>
2025-11-25 21:34:23 +0100

    add Title

---
+++ src/main/java/hr/tpopovic/myshowlist/application/domain/model/Title.java
@@ -0,0 +1,17 @@
+package hr.tpopovic.myshowlist.application.domain.model;
+
+import static java.util.Objects.requireNonNull;
+
+public record Title(String name) {
+
+    public Title {
+        requireNonNull(name, "name must not be null");
+        if (name.isBlank()) {
+            throw new IllegalArgumentException("name must not be blank");
+        }
+        if (name.length() > 255) {
+            throw new IllegalArgumentException("name must not be longer than 255 characters");
+        }
+    }
+
+}
