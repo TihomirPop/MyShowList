60ec774918b1f661c906472714123549109245bb
TihomirPop <tihomirpopovic42@gmail.com>
2025-11-28 16:50:41 +0100

    Wire dummy implementations to make application startable

--- src/main/java/hr/tpopovic/myshowlist/config/DomainConfig.java
+++ src/main/java/hr/tpopovic/myshowlist/config/DomainConfig.java
@@ -1,9 +1,12 @@
 package hr.tpopovic.myshowlist.config;
 
+import hr.tpopovic.myshowlist.application.domain.model.Token;
+import hr.tpopovic.myshowlist.application.domain.model.Username;
 import hr.tpopovic.myshowlist.application.domain.service.AuthService;
 import hr.tpopovic.myshowlist.application.domain.service.ShowService;
 import hr.tpopovic.myshowlist.application.port.in.FetchShows;
 import hr.tpopovic.myshowlist.application.port.in.RegisterUser;
+import hr.tpopovic.myshowlist.application.port.out.FetchPasswordHashResult;
 import hr.tpopovic.myshowlist.application.port.out.ForHashingPassword;
 import hr.tpopovic.myshowlist.application.port.out.ForLoadingShows;
 import hr.tpopovic.myshowlist.application.port.out.ForSavingUser;
@@ -20,6 +23,12 @@ public class DomainConfig {
 
     @Bean
     public RegisterUser registerUser(ForHashingPassword forHashingPassword, ForSavingUser forSavingUser) {
-        return new AuthService(forHashingPassword, forSavingUser);
+        return new AuthService(
+                forHashingPassword,
+                forSavingUser,
+                        (_) -> new FetchPasswordHashResult.Failure(),
+                (_, __) -> false,
+                (Username _) -> new Token("dummy-token")
+        );
     }
 }
