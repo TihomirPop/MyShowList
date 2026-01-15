3fd69f13a16aba7901d0dfb0c8bb69373beb94c4
TihomirPop <tihomirpopovic42@gmail.com>
2026-01-15 19:17:54 +0100

    wire admin show components in config

--- src/main/java/hr/tpopovic/myshowlist/config/DomainConfig.java
+++ src/main/java/hr/tpopovic/myshowlist/config/DomainConfig.java
@@ -4,7 +4,10 @@ import hr.tpopovic.myshowlist.application.domain.service.AuthService;
 import hr.tpopovic.myshowlist.application.domain.service.ReviewService;
 import hr.tpopovic.myshowlist.application.domain.service.ShowService;
 import hr.tpopovic.myshowlist.application.domain.service.UserShowService;
+import hr.tpopovic.myshowlist.application.port.in.CreateShow;
+import hr.tpopovic.myshowlist.application.port.in.DeleteShow;
 import hr.tpopovic.myshowlist.application.port.in.FetchShows;
+import hr.tpopovic.myshowlist.application.port.in.UpdateShow;
 import hr.tpopovic.myshowlist.application.port.out.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,8 +16,42 @@ import org.springframework.context.annotation.Configuration;
 public class DomainConfig {
 
     @Bean
-    public FetchShows fetchShows(ForLoadingShows forLoadingShows, ForFetchingScore forFetchingScore) {
-        return new ShowService(forLoadingShows, forFetchingScore);
+    public ShowService showService(
+            ForLoadingShows forLoadingShows,
+            ForFetchingScore forFetchingScore,
+            ForLoadingGenres forLoadingGenres,
+            ForSavingShow forSavingShow,
+            ForUpdatingShow forUpdatingShow,
+            ForDeletingShow forDeletingShow
+    ) {
+        return new ShowService(
+                forLoadingShows,
+                forFetchingScore,
+                forLoadingGenres,
+                forSavingShow,
+                forUpdatingShow,
+                forDeletingShow
+        );
+    }
+
+    @Bean
+    public FetchShows fetchShows(ShowService showService) {
+        return showService;
+    }
+
+    @Bean
+    public CreateShow createShow(ShowService showService) {
+        return showService;
+    }
+
+    @Bean
+    public UpdateShow updateShow(ShowService showService) {
+        return showService;
+    }
+
+    @Bean
+    public DeleteShow deleteShow(ShowService showService) {
+        return showService;
     }
 
     @Bean
