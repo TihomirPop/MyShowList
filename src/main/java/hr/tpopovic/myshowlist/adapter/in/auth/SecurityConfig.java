1e902b0474537294599b9032cfad486ff96791f1
TihomirPop <tihomirpopovic42@gmail.com>
2026-01-15 19:15:56 +0100

    add admin show controller with authorization

--- src/main/java/hr/tpopovic/myshowlist/adapter/in/auth/SecurityConfig.java
+++ src/main/java/hr/tpopovic/myshowlist/adapter/in/auth/SecurityConfig.java
@@ -3,6 +3,7 @@ package hr.tpopovic.myshowlist.adapter.in.auth;
 import hr.tpopovic.myshowlist.application.port.in.ValidateToken;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -10,6 +11,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
