65ec3be6f733ef0693699d6c27c7d4ae98b70535
TihomirPop <tihomirpopovic42@gmail.com>
2026-01-15 16:58:55 +0100

    add UserId to Review domain model

--- src/main/java/hr/tpopovic/myshowlist/application/domain/model/Review.java
+++ src/main/java/hr/tpopovic/myshowlist/application/domain/model/Review.java
@@ -3,11 +3,13 @@ package hr.tpopovic.myshowlist.application.domain.model;
 import static java.util.Objects.requireNonNull;
 
 public record Review(
+        UserId userId,
         ShowId showId,
         ReviewText reviewText
 ) {
 
     public Review {
+        requireNonNull(userId, "userId");
         requireNonNull(showId, "showId");
         requireNonNull(reviewText, "reviewText");
     }
