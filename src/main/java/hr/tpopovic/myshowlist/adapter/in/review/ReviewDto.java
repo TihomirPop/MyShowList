488b4f389f862ca7ebbff0ab11347357854e229d
TihomirPop <tihomirpopovic42@gmail.com>
2026-01-15 17:15:39 +0100

    implement ReviewController with CRUD operations

--- src/main/java/hr/tpopovic/myshowlist/adapter/in/review/ReviewDto.java
+++ src/main/java/hr/tpopovic/myshowlist/adapter/in/review/ReviewDto.java
@@ -1,5 +1,10 @@
 package hr.tpopovic.myshowlist.adapter.in.review;
 
-public record ReviewDto(String review) {
-
+public record ReviewDto(
+        String reviewText,
+        String username
+) {
+    public ReviewDto(String reviewText) {
+        this(reviewText, null);
+    }
 }
