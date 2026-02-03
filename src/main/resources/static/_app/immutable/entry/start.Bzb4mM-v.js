54479f1c648d6511a95dd84369ca2044d397573a
TihomirPop <tihomirpopovic42@gmail.com>
2026-02-03 17:49:04 +0100

    update static frontend

--- src/main/resources/static/_app/immutable/entry/start.Bzb4mM-v.js
+++
@@ -1,1 +0,0 @@
-import{aW as o,aV as r}from"../chunks/DDMLW2bY.js";export{o as load_css,r as start};
---
+++ src/main/resources/static/_app/immutable/entry/start.CS3k5wET.js
@@ -0,0 +1,1 @@
+import{aW as o,aV as r}from"../chunks/DT7DyGpg.js";export{o as load_css,r as start};
